0329565ee5d732bf25f1223316379c7f009a298a
Joshua <joshallen06@gmail.com>
2021-04-25 16:25:45 +0000

    creating master

---
+++ tests/tests.py
@@ -0,0 +1,14 @@
+"""
+write at least three tests for the English to French translator
+test for null input
+"""
+
+
+"""
+write at least 3 tests for the English to German translator
+test for null input
+"""
+
+"""
+run pylint against the python code and score at least 7 or higher
+"""
---
+++ translator.py
@@ -0,0 +1,11 @@
+def english_to_french(text_input):
+    """
+    This function translates from english to french
+    """
+    return 
+
+def english_to_german(text_input):
+    """
+    this function translates english to german
+    """
+    return
